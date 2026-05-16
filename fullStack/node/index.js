4a042e7adfae92dde4e48bfbff5e9ad35e5a6488
Tech-FireFish <baihongc732@gmail.com>
2026-05-16 10:12:06 +0800

    Refactor fetch calls and update function names

--- fullStack/node/index.js
+++ fullStack/node/index.js
@@ -1,24 +1,28 @@
 
 import {} from "./data.js";
 
-// fetch('https://apis.scrimba.com/bored/api/activity')
-//     .then(response=>response.json())
-//     .then(data=> console.log(data))
-//     .then(data=> {
-        
-//     })
-// fetch('https://dog.ceo/api/breeds/image/random')
+// fetch('https://apis.scrimba.com/dog.ceo/api/breeds/image/random')
 //     .then(response => response.json())
-//     .then(data => {
-//         const imageElement = document.createElement('img');
-//         imageElement.src=data.message;
-//         console.log(imageElement);
-//         imageElement.alt='ran';
-//         document.getElementById('img-container').appendChild(imageElement);
+//     .then(c=>{
+//         console.log(c);
+//         const imageContainer = document.createElement('img');
+//         imageContainer.src=c.message;
+//         document.getElementById('image-container').appendChild(imageContainer)
 //     })
-async function getStatus(){
-    const response = await fetch('https://apis.scrimba.com/bored/api/activity');
-    const data = await response.json()
+
+// fetch('https:apis.scrimba.com/bored/api/activity')
+//     .then(response=>response.json())
+//     .then(data=>console.log(data.activity))
+//     .then(data=>{
+//         const board = document.createElement('h1');
+//         board.textContent = data.activity;
+//         document.getElementById('image-container').appendChild(board);
+//     })
+
+async function getActivity(){    
+    const response = await fetch('https:apis.scrimba.com/bored/api/activity');
+    const data = await response.json();
     console.log(data);
+
 }
-getStatus();
+getActivity();
