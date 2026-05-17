c39f64f4530f6e0acb98279c362d4567c1d0791f
Tech-FireFish <baihongc732@gmail.com>
2026-05-17 22:03:47 +0800

    Refactor API calls and error handling

--- fullStack/node/API basics/index.js
+++ fullStack/node/API basics/index.js
@@ -76,29 +76,132 @@ import {} from "./data.js";
 //     console.log('The operation has completed!')
 // }
 
-try{
-    const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts',{
-        method:'POST',
-        body: JSON.stringify({
-            title:'mysterious box',
-            body:'undefined',
-            userId:1000,
-        }),
-        headers:{
-            'Content-Type':'application/json',
-        },
-    });
-    if(!response.ok){
-        console.log('Endpoint Error');
-    }; 
-    const data = await response.json();
-    console.log(data); 
-}catch(err){
-    console.log(err);  
-}finally{
-    console.log('The operation has completed!')
-}
-///////////
-//PART II//
-///////////
+// try{
+//     const response = await fetch('https://apis.scrimba.com/jsonplaceholder/posts',{
+//         method:'POST',
+//         body: JSON.stringify({
+//             title:'mysterious box',
+//             body:'undefined',
+//             userId:1000,
+//         }),
+//         headers:{
+//             'Content-Type':'application/json',
+//         },
+//     });
+//     if(!response.ok){
+//         console.log('Endpoint Error');
+//     }; 
+//     const data = await response.json();
+//     console.log(data); 
+// }catch(err){
+//     console.log(err);  
+// }finally{
+//     console.log('The operation has completed!')
+// }
+///////////////////
+//PART II-Promise//
+///////////////////
+
+// const promise = new Promise((resolve, reject)=>{
+//     const success= Math.random() > 0.5
+//     if(success){
+//         resolve('Operation successful')
+//     } else{
+//         reject('Operation failed')
+//     }
+// });
+// //METHOD1: promise.then(response => console.log(response))
+// //METHOD2:
+// try{
+//     const response = await promise;
+//     console.log(response);
+// }catch(err){
+//     console.log(err);
+// }
+
+// const promise = new Promise((resolve, reject)=>{
+//     const success = Math.random() > 0.5;
+//     if (success){
+//         resolve('Operation successful');
+//     } else{
+//         reject('Operation failed');
+//     }
+// });
+// try{
+//     const response = await promise;
+//     console.log(response);
+// }catch(err){
+//     console.log(err);
+// }
+
+// function preLoading(url){
+//     return new Promise((resolve, reject)=>{
+//         const img = new Image();
+//         img.src=url;
+//         img.alt='Landscape Image Failed';
+//         img.addEventListener('load',()=>resolve(img));
+//         img.addEventListener('error',()=>reject('Image has NOT loaded'));
+//     })
+// }
+// try{
+//     const results = await preLoading('https://scrimba.ams3.cdn.digitaloceanspaces.com/assets/courses/gadvancedjs/scenic1.jpg');
+//     console.log(results);
+//     document.getElementById('image-container').appendChild(results);
+// }catch(err){
+//     console.log(err);
+// }
+
+// function upload(){
+//     return new Promise((resolve,reject)=>{
+//         console.log('STEP1: Uploading file...');
+//         setTimeout(()=>{
+//             resolve()
+//         },1000)
+//     })
+// }
+// function process(){
+//     return new Promise((resolve,reject)=>{
+//         console.log('STEP1: Processing file...');
+//         setTimeout(()=>{
+//             resolve()
+//         },1000)
+//     })
+// }
+// function notify(){
+//     return new Promise((resolve,reject)=>{
+//         console.log('STEP1: Notifying user...');
+//         setTimeout(()=>{
+//             resolve()
+//         },1000)
+//     })
+// }
+// try{
+//     await upload();
+//     await process();
+//     await notify();
+//     await console.log('All tasks completed.')
+// }catch(err){
+//     console.log(err);
+// }
+
+// function createPromise(){
+//     return new Promise((resolve,reject)=>{
+//         const success = Math.random() > 0.5;
+//         if(success){
+//             resolve('Operation successful');
+//         }else{
+//             reject('Operation failed.');
+//         }
+        
+//     });
+// }
+// try{
+//     const promise1 = createPromise();
+//     const promise2 = createPromise();
+//     const promise3 = createPromise();
+//     const result = await Promise.all([promise1,promise2,promise3]);
+//     console.log(result)
+// }catch(err){
+//     console.log(err);
+// }
 
