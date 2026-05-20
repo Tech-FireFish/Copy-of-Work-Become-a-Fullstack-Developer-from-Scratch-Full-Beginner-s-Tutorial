fd7df51d09a0dbef0673a3571fb1e5eb04bbd451
Tech-FireFish <baihongc732@gmail.com>
2026-05-20 09:27:14 +0800

    initial files for project wild-horizons

---
+++ fullStack/node/wild-horizons/server.js
@@ -0,0 +1,11 @@
+//type in "npm init" in the terminal
+////////
+//TIPS//
+////////
+//if not working: 
+//type in "get-executionpolicy"
+//if output: Restricted
+//type in "get-executionpolicy -scope currentuser"
+//enter "remotesigned"
+//
+console.log("The server is running!")
