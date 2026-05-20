7d0dfca90593bbffefc1db38e164e2908a115bef
Tech-FireFish <baihongc732@gmail.com>
2026-05-21 07:36:05 +0800

    Implement basic HTTP server with API endpoint
    Refactor server.js to include HTTP server setup and API endpoint handling.

--- fullStack/node/wild-horizons/server.js
+++ fullStack/node/wild-horizons/server.js
@@ -8,4 +8,37 @@
 //type in "get-executionpolicy -scope currentuser"
 //enter "remotesigned"
 //
-console.log("The server is running!")
+import http from 'node:http';
+console.log("The server is running!");
+
+// const PORT = 8000;
+// const server = http.createServer((req,res)=>{
+//     res.end('The request is successful!');
+// });
+// server.listen(PORT, ()=>console.log(`server running on port: ${PORT}`));
+
+// import http from 'node:http'
+// const PORT = 8000;
+// const server = http.createServer((req,res) => {
+//     res.write('This is some data \n');
+//     res.write('This is some more data \n');
+//     res.end('The server runs successfully! \n','utf8',()=>console.log('response end'));
+//     res.end();
+// })
+// server.listen(PORT,()=>{
+//     console.log(`The request was successful! on port: ${PORT}`);
+// });
+
+const PORT = 8000;
+const server = http.createServer((req,res)=>{
+
+    if(req.url === '/api' && req.method === 'GET'){
+        console.log(req.url);
+    res.end('The request is successful!');
+    }else{
+
+    }
+   
+});
+server.listen(PORT, ()=>console.log(`server running on port: ${PORT}`));
+
